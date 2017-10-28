4813b66b17ae4d8894dbe46c1ad0909c20106caa
HarryStevens <harryjosephstevens@gmail.com>
2017-10-28 11:03:53 +0530

    Initial release

---
+++ index.js
@@ -0,0 +1,92 @@
+/*
+@license: MIT License
+
+Copyright (c) 2017 Harry Stevens
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+var output = (function(){
+	function shape2path(){
+		this.circle = circle;
+		this.rect = rect;
+	}
+
+	function circle(options) {
+		if (!options){
+			throw Error("You must pass options to shape2path.circle()");
+		} else if (options){
+			if (!options.cx) {
+				throw Error("You must pass cx as an opiton to shape2path.circle()");
+			} else if (!options.cy) {
+				throw Error("You must pass cy as an opiton to shape2path.circle()");
+			} else if (!options.r) {
+				throw Error("You must pass r as an opiton to shape2path.circle()");
+			}
+		}
+
+		var cx = options.cx, cy = options.cy, r = options.r;
+		return "M" + cx + "," + cy + " m" + (-r) + ",0 a" + r + "," + r + " 0 1,0 " + (r * 2) + ",0 a" + r + "," + r + " 0 1,0 " + (-r * 2) + ",0";
+	}
+
+	function rect(options){
+		if (!options){
+			throw Error("You must pass options to shape2path.rect()");
+		} else if (options){
+			if (!options.x) {
+				throw Error("You must pass x as an opiton to shape2path.rect()");
+			} else if (!options.y) {
+				throw Error("You must pass y as an opiton to shape2path.rect()");
+			} else if (!options.width) {
+				throw Error("You must pass width as an opiton to shape2path.rect()");
+			} else if (!options.height){
+				throw Error("You must pass height as an opiton to shape2path.rect()");
+			}
+		}
+
+		var x = options.x, y = options.y, w = options.width + x, h = options.height + y;
+
+		if (options.rx || options.ry){
+			var rx = options.rx ? options.rx : options.ry,
+				ry = options.ry ? options.ry : options.rx;
+
+			return "M" + (x + rx) + "," + y + 
+		  	" H" + (w - rx) +
+		  	" C" + (w - rx) + "," + y + " " + w + "," + y + " " + w + "," + (y + ry) + 
+		  	" V" + (h - ry) + 
+		  	" C" + w + "," + (h - ry) + " " + w + "," + h + " " + (w - rx) + "," + h +
+		  	" H" + (x + rx) +
+		  	" C" + (x + rx) + "," + h + " " + x + "," + h + " " + x + "," + (h - ry) +
+		  	" V" + (y + ry) +
+		  	" C" + x + "," + (y + ry) + " " + x + "," + y + " " + (x + rx) + "," + y;
+		} else {
+			return "M" + x + "," + y + " H" + w + " V" + h + " H" + x + " Z";
+		}
+
+	}
+
+	return new shape2path;
+		
+}());
+
+if (typeof window === "undefined") {
+  module.exports = output;
+} else {
+  window.shape2path = output;
+}
---
+++ test/test.js
@@ -0,0 +1,20 @@
+var expect = require("chai").expect;
+var shape2path = require("../index");
+
+describe("#shape2path", function() {
+	it ("should convert a circle to a path", function(){
+		expect(shape2path.circle({cx: 65, cy: 20, r: 20})).to.equal("M65,20 m-20,0 a20,20 0 1,0 40,0 a20,20 0 1,0 -40,0");
+	});
+
+	it ("should convert a rect to a path", function(){
+		expect(shape2path.rect({x: 45, y: 70, width: 40, height: 40})).to.equal("M45,70 H85 V110 H45 Z");
+	});
+
+	it ("should convert a rounded rect to a path", function(){
+		expect(shape2path.rect({x: 220, y: 120, width: 200, height: 200, rx: 20, ry: 10})).to.equal("M240,120 H400 C400,120 420,120 420,130 V310 C420,310 420,320 400,320 H240 C240,320 220,320 220,310 V130 C220,130 220,120 240,120");
+	});
+
+	it ("should convert a rounded rect with only one rounded attribute specified to a path", function(){
+		expect(shape2path.rect({x: 120, y: 340, width: 100, height: 100, rx: 15})).to.equal("M135,340 H205 C205,340 220,340 220,355 V425 C220,425 220,440 205,440 H135 C135,440 120,440 120,425 V355 C120,355 120,340 135,340");
+	});
+});
