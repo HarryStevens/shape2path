7094ba9c59d160e6b988a870b57b1e67b3176d1e
HarryStevens <harryjosephstevens@gmail.com>
2017-10-30 12:00:08 +0530

    Add both empty rx and empty ry to rounded rect test

--- test/test.js
+++ test/test.js
@@ -14,8 +14,12 @@ describe("#shape2path", function() {
 		expect(shape2path.rect({x: 220, y: 120, width: 200, height: 200, rx: 20, ry: 10})).to.equal("M240,120 H400 C400,120 420,120 420,130 V310 C420,310 420,320 400,320 H240 C240,320 220,320 220,310 V130 C220,130 220,120 240,120");
 	});
 
-	it ("should convert a rounded rect with only one rounded attribute specified to a path", function(){
+	it ("should convert a rounded rect with only rx specified to a path", function(){
 		expect(shape2path.rect({x: 120, y: 340, width: 100, height: 100, rx: 15})).to.equal("M135,340 H205 C205,340 220,340 220,355 V425 C220,425 220,440 205,440 H135 C135,440 120,440 120,425 V355 C120,355 120,340 135,340");
+	});
+
+	it ("should convert a rounded rect with only ry specified to a path", function(){
+		expect(shape2path.rect({x: 120, y: 340, width: 100, height: 100, ry: 15})).to.equal("M135,340 H205 C205,340 220,340 220,355 V425 C220,425 220,440 205,440 H135 C135,440 120,440 120,425 V355 C120,355 120,340 135,340");
 	});
 
 	it ("should convert an ellipse to a path", function(){
